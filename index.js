24b62e61a52f0c546e44cad809afe5b1a77966dc
温施琦su <wenshiqi@bytedance.com>
2022-05-18 14:29:40 +0800

    fix workflow

--- index.js
+++ index.js
@@ -124,11 +124,17 @@ if (!general_pass)throw "缺少信息门户密码"
 time = process.env["BUPT_TIME"]
 if (!time)throw "缺少时段"
 var next_date = new Date()
-next_date.setDate(new Date().getDate()+3)//预约最新一天
+next_date.setDate(new Date().getDate())//预约最新一天
 next_date = date.format(next_date,"YYYYMMDD")
 
 core.exportVariable('APPOINT_DATE', next_date);
-hour = (time==="1")?"13:00-14:30":((time==="2")?"19:40-20:40":(time==="3"?"20:40-21:40":"未知"))
+console.log("time 是.....",time)
+hour = ""
+if (time==="1"){
+   hour="13:00-14:30"
+}else{
+    hour="未知"
+}
 core.exportVariable('APPOINT_HOUR', hour);
 console.log("将预约"+next_date+"第"+process.env.APPOINT_HOUR+"时段")
 
